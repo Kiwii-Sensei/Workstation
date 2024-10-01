61c73b3fb641ed901df221dbfd1cae3937040811
Corey Lambert <therudereaper23@hotmail.com>
2024-10-01 11:52:20 -0400

    Adding C++

---
+++ C++/Week2/week2.cpp
@@ -0,0 +1,40 @@
+
+#include<iostream>
+
+using namespace std;
+
+namespace CST8219 {
+	class Vehicle 
+	{
+	private:
+		int numWheels, numDoors;
+
+	public:
+		Vehicle(int w, int d)
+		{
+			numWheels = w;
+			numDoors = d;
+			cout << "In constructor with 2 parameters" << endl;
+		}
+		Vehicle(int w) : Vehicle(w, 4)
+		{
+			cout << "In constructor with 1 parameters, wheels = " << w << endl;
+		}
+		Vehicle() : Vehicle(4)
+		{
+			cout << "In constructor with 0 parameters" << endl;
+		}
+		Vehicle :: ~Vehicle() 
+		{
+			cout << "In destructor" << endl;
+		}
+			
+	};
+}
+
+int main(int argc, char **argv)
+{
+	CST8219::Vehicle myVehicle;
+	cout << "I made a Vehicle!" << endl;
+	return 0;
+}
